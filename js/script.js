91c10551863012e9a8dc5aead6126fdeb7b75647
Damian Michalowski <dowski888@gmail.com>
2022-02-12 17:39:53 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+{
+    const welcome = () => {
+        console.log("Witam wszystki developerów zaglądających tutaj");
+    };
+
+    const FormOnSumbit = (event) => {
+        event.preventDefault();
+
+        const sumElement = document.querySelector(".js-sum");
+        const rateElement = document.querySelector(".js-rate");
+
+        const sum = sumElement.value;
+        const rate = rateElement.value;
+        const result = sumCountuing(sum, rate);
+        sumResultUpdate(result, sum);
+    }
+
+    const sumCountuing = (sum, rate) => {
+        return sum * rate;
+    }
+
+    const sumResultUpdate = (result, sum) => {
+        const resultElement = document.querySelector(".js-result");
+        const sumResult = document.querySelector(".js-sumResult");
+
+        resultElement.innerText = result.toFixed(2);
+        sumResult.innerText = sum;
+    }
+
+    const reset = () => {
+        const resultElement = document.querySelector(".js-result");
+        const sumResult = document.querySelector(".js-sumResult");
+
+        resultElement.innerText = "0.00";
+        sumResult.innerText = "0.00";
+    }
+
+    const currencyElementTextChanging = () => {
+        const currencyElement = document.querySelector(".js-currency");
+        const currencyNameElement = document.querySelector(".js-currencyName");
+        const currencyResult = document.querySelector(".js-currencyResult");
+        //drugi raz deklaruje zmienną rateElement do poprawki
+        const rateElement = document.querySelector(".js-rate");
+
+        if (currencyElement.value === 'USD') {
+            currencyNameElement.innerText = `Podaj kwotę(USD)*`;
+            currencyResult.innerText = `USD`;
+            rateElement.value = 4.0049;
+        }
+    
+        if (currencyElement.value === 'EUR') {
+            currencyNameElement.innerText = `Podaj kwotę(EUR)*`;
+            rateElement.value = 4.5358;
+            currencyResult.innerText = `EUR`;
+        }
+    }
+
+    const init = () => {
+        const buttonResetElement = document.querySelector(".js-buttonReset");
+        buttonResetElement.addEventListener("click", reset);
+
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", FormOnSumbit);
+        formElement.addEventListener("input", currencyElementTextChanging);
+        welcome();
+    }
+
+    init();
+}
